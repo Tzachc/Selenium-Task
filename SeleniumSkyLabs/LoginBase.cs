19bca699b4ae71ff209a23e3c6f1819e938f34b1
Tzach Cohen <57837594+Tzachc@users.noreply.github.com>
2022-08-29 17:02:23 +0300

    Add files via upload

---
+++ SeleniumSkyLabs/LoginBase.cs
@@ -0,0 +1,85 @@
+﻿using OpenQA.Selenium;
+using System;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace SeleniumTask
+{
+    public class LoginBase
+    {
+        protected IWebDriver driver;
+
+        public LoginBase() //constructor.
+        {
+            // create driver for chrome browser.
+            driver = new ChromeDriver();
+
+        }
+        public void Init()
+        {
+            // Navigate to the site.
+            String urlFromExcel = FindInfoFromExcel("URL"); // Search for the URL in excel file.
+            driver.Url = urlFromExcel;
+            driver.Manage().Window.Maximize(); // Maximize the browser.
+
+        }
+
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+        
+        // Function to extract data (User name & Password) from Excel sheet.
+        public String FindInfoFromExcel(String info)
+        {
+            try
+            {
+                
+                String[] listB = new String[3];
+                String user = Environment.UserName;
+                String[] paths = { @"C:\Users\", user, "source","repos", "SeleniumSkyLabs","SeleniumSkyLabs","Information.csv" };
+                String fullPath = Path.Combine(paths);
+                
+                using (var reader = new StreamReader(@fullPath)) 
+                {
+                    int counter = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+                        if (counter >= 0)
+                        {
+                            listB[0] = values[1]; //UserName
+                            listB[1] = values[2]; //Password
+                            listB[2] = values[0]; //URL
+                        }
+                        counter++;
+                    }
+
+                }
+                if (info.Equals("UserName"))
+                {
+                    return listB[0];
+                }
+                else if (info.Equals("Password"))
+                {
+                    return listB[1];
+                }
+                else if (info.Equals("URL"))
+                {
+                    return listB[2];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception at: " + ex.Message);
+                return null;
+            }
+        }
+
+    }
+}
